16093d7c6c2dbe8e12755383854311d62d6a0ede
Boni1995 <francobonifacini@hotmail.com>
2023-12-05 10:36:05 +0100

    ok

--- R Project - Decoding Red and White Wine Quality.R
+++ R Project - Decoding Red and White Wine Quality.R
@@ -7,5 +7,13 @@ getwd()
 file.choose()
 
 # Importing the wine dataset and printing only some rows
-df_wine <- read.csv('C:\\Users\\franc\\Documents\\GitHub\\rproject\\Dataset\\winequality.csv')
-head(df_wine)
+wine_dataset <- read.csv('C:\\Users\\franc\\Documents\\GitHub\\rproject\\Dataset\\winequality.csv')
+head(wine_dataset)
+
+# Checking elements inside the dataset
+str(wine_dataset) # First attribute is char, the next 11 attributes are numeric, and the last one is integer.
+
+# Check if there is any NULL values
+nulls_summary <- data.frame(NullValues = colSums(is.na(wine_dataset)))
+
+nulls_summary
